318bd3d83800abcc1c0faeb55e592c4b54c7d709
Mohammed47107 <148840891+Mohammed47107@users.noreply.github.com>
2025-06-28 22:46:09 +0000

    si

--- Carpeta B/c++/punteros/punteros.cpp
+++ Carpeta B/c++/punteros/punteros.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
+    int *punt,i;
+    int x[5] = {1, 2, 3, 6, 8};
+    punt=&x[0];
+    // Imprimir los valores del array usando el puntero
+    cout << "Valores del array usando el puntero:" << endl;
+    for(i=0; i<5; i++){
+        cout << *(punt+i) << endl;
+    }
     cout << "Hola" << endl;
     return 0;
 }
