a1f73f12a56e7c453afd45f5be2500d6034af772
Mohammed47107 <148840891+Mohammed47107@users.noreply.github.com>
2025-06-28 15:44:06 +0000

    Add C++ build task configuration and initial array example

---
+++ Carpeta B/c++/array/arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// λ
+
+
+int main(){
+    int x;
+    // ingreso y prints
+    cout << "Ingrese un valor: " << endl;
+    cin >> x;
+    cout << "El valor ingresado es: " << x << endl;    
+    return 0;
+}
